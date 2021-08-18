4895dcf1aa31fc83c4a8cc989b65a285a24dabe0
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2021-08-18 19:12:12 +0530

    Update twoPointers.cpp

--- twoPointers.cpp
+++ twoPointers.cpp
@@ -53,3 +53,21 @@ vector<vector<int>> threeSum(vector<int>& nums) {
                 }
         return ans;
         }
+
+
+// remove duplicates from sorted array
+
+int removeDuplicates(vector<int>& nums) {
+        int j=0;
+        if(nums.size()==0){
+            return 0;
+        }
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        nums[j]=nums[nums.size()-1];
+        return j+1;
+    }
