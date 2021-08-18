171854bd048c1f5169630a9fa62b67554f2df4d0
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2021-08-18 18:01:06 +0530

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -150,7 +150,39 @@ ListNode* rotateRight(ListNode* head, int k) {
     return head;
 }
 
+//Deep copy
 
+
+Node* copyRandomList(Node* head) {
+    Node* first = head;
+    Node* second = head;
+    while(first!=NULL){
+        second = first->next;
+
+        Node* copy = new Node(first->val);
+        first->next = copy;
+        copy->next = second;
+        first = second;
+    }
+    first = head;
+    while(first!=NULL){
+        if(first->random!=NULL){
+            first->next->random  = first->random->next;
+        }
+        first = first->next->next;
+    }
+    first = head;
+    Node* dummy = new Node(0);
+    Node* copy = dummy;
+    while(first!=NULL){
+        second = first->next->next;
+        copy->next = first->next;
+        first->next = second;
+        copy = copy->next;
+        first = first->next;
+    }
+    return dummy->next;
+}
 
 // #include <iostream>
 // using namespace std;
