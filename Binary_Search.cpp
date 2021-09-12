ed487438e2f163f66403cf327c6ff90c7b645485
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2021-09-12 17:01:27 +0530

    Update Binary_Search.cpp

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -26,3 +26,24 @@ int countSmaller(vector<int> &arr, int key){
       }
       return low;
   }
+
+//Find the element that appears once in sorted array, and rest element appears twice 
+
+/*You need to find the left half---where first instance is at even index and second instance is at odd index*/
+
+
+int singleNonDuplicate(vector<int>& nums) {
+  int n = nums.size();
+  int low = 0, high = n-2;
+  while(low<=high){
+      int mid = (low+high)/2;
+      if(mid%2==0){
+          if(nums[mid+1]==nums[mid]) low = mid+1;
+          else high = mid-1;
+      }else{
+          if(nums[mid-1]==nums[mid]) low = mid+1;
+          else high = mid-1;
+      }
+  }
+  return nums[low];
+}
