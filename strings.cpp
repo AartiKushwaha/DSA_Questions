b13d338db4a9786d9f973703863c31523d88ef20
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2021-10-04 13:07:43 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -14,3 +14,23 @@ string reverseWords(string s) {
 
       return result;
   }
+
+//https://leetcode.com/problems/longest-palindrome/
+int longestPalindrome(string s) {
+        int n = s.size();
+        unordered_map<char,int> mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
+        int ans = 0, flag1 = 0;
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if((it->second) % 2 == 0) ans+=it->second;
+            else if(it->second == 1) flag1 = 1;
+            else if(it->second % 2 == 1){
+                ans+=(it->second)-1;
+                flag1=1;
+            } 
+        }
+        if(flag1==1) ans+=1;
+        return ans;
+      }
