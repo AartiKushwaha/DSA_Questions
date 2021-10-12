352a0af7ebc5973c338424ad39ab729736792962
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2021-10-12 19:55:10 +0530

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -1,4 +1,4 @@
-//BFS    ----> Connected components, nodes starting with zero
+//BFS    ---->for disconnected components, nodes starting with zero
 
 class Solution {
   public:
@@ -25,5 +25,28 @@ class Solution {
             }
         }
         return bfsRes;
+    }
+};
+
+
+//DFS   ---->for Connected components
+
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    
+    void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &ans){
+        ans.push_back(node);
+        vis[node]=1;
+        for(auto it: adj[node]){
+            if(!vis[it]) dfs(it, vis, adj, ans);
+        }
+    }
+    
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> dfsRes;
+        vector<int> vis(V,0);
+        dfs(0,vis,adj,dfsRes);
+        return dfsRes;
     }
 };
