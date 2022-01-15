9ca35e3e7f925607bb7497c8315c8cb0ec4f8265
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-01-15 19:56:16 +0530

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -125,3 +125,38 @@ public:
 	}
 
 };
+
+//Topological Sort using DFS
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	
+	void topo(int node, vector<int> adj[], vector<int> &vis, stack<int> &st){
+	    vis[node]  = 1;
+	    for(auto it: adj[node]){
+	        if(!vis[it]){
+	            topo(it, adj, vis, st);
+	        }
+	    }
+	    st.push(node);
+	}
+	
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> vis(V, 0);
+	    vector<int> ans;
+	    stack<int> st;
+	    for(int i=0; i<V; i++){
+	        if(!vis[i]){
+	            topo(i, adj, vis, st);
+	        }
+	    }
+	    while(!st.empty()){
+	        ans.push_back(st.top());
+	        st.pop();
+	    }
+	    return ans;
+	}
+};
