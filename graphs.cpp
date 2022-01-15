26cf2160fea872fe62345fa487fba8770c8f6ad1
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-01-15 20:51:38 +0530

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -160,3 +160,37 @@ class Solution
 	    return ans;
 	}
 };
+
+
+//Topo. sort using BFS (KAHN'S ALGO)
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> ans;
+	    vector<int> indegree(V,0);
+	    for(int i=0;i<V;i++){
+	        for(auto it: adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0) q.push(i);
+	    }
+	    while(!q.empty()){
+	        int node = q.front();
+	        ans.push_back(node);
+	        q.pop();
+	        for(auto it: adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0) q.push(it);
+	        }
+	    }
+	    return ans;
+	}
+};
