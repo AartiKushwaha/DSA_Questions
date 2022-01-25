652f483264aac231c6f6310005eef2b32262f14b
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-01-25 13:14:31 +0530

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -590,33 +590,24 @@ int main(){
 
 class UnionFind {
 public:
-    UnionFind(int sz) : root(sz), rank(sz) {
+    UnionFind(int sz) : root(sz) {
         for (int i = 0; i < sz; i++) {
             root[i] = i;
-            rank[i] = 1;
         }
     }
 
     int find(int x) {
-        while (x != root[x]) {
-            x = root[x];
+        if (x == root[x]) {
+            return x;
         }
-        return x;
-	//return root[x] = find(root[x]);     path compressions---> TC get O(logn)
+        return root[x] = find(root[x]);
     }
 
     void unionSet(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
         if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
+            root[rootY] = rootX;
         }
     }
 
@@ -626,7 +617,6 @@ public:
 
 private:
     vector<int> root;
-    vector<int> rank;
 };
 
 // Test Case
