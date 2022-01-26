707278f93b4e2aa29cb34469584c2534ee191747
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-01-26 19:05:23 +0530

    Update graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -610,6 +610,24 @@ public:
             root[rootY] = rootX;
         }
     }
+	
+/*UNION BY RANK*/
+//     void unionSet(int x, int y) {
+//         int rootX = find(x);
+//         int rootY = find(y);
+//         if (rootX != rootY) {
+//             if (rank[rootX] > rank[rootY]) {
+//                 root[rootY] = rootX;
+//             } else if (rank[rootX] < rank[rootY]) {
+//                 root[rootX] = rootY;
+//             } else {
+//                 root[rootY] = rootX;
+//                 rank[rootX] += 1;
+//             }
+//         }
+//     }
+	
+	
 
     bool connected(int x, int y) {
         return find(x) == find(y);
