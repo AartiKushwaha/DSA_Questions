2bee411a547dbaf71793fdd13f1fdeecf6c12085
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-02-26 12:10:34 +0530

    Update Binary_Trees.cpp

--- Binary_Trees.cpp
+++ Binary_Trees.cpp
@@ -67,6 +67,36 @@ public:
         return ans;
     }
 };
+
+//Using one stack
+
+ vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        if(root == NULL) return postorder;
+        stack<TreeNode*> st1;
+        TreeNode* current = root;  
+        while(current != NULL || !st1.empty()) {
+            if(current != NULL){
+                st1.push(current);
+                current = current->left;
+            }else{
+                TreeNode* temp = st1.top()->right;
+                if (temp == NULL) {
+                    temp = st1.top();
+                    st1.pop(); 
+                    postorder.push_back(temp->val);
+                    while (!st1.empty() && temp == st1.top()->right) {
+                        temp = st1.top();
+                        st1.pop(); 
+                        postorder.push_back(temp->val);
+                    }
+                } else {
+                    current = temp;
+                }
+            }
+        }
+        return postorder;
+    }
 vector<int> postorderTraversal(TreeNode* root) {
     if(root==NULL) return {};
     stack<pair<TreeNode*, int>> st;
