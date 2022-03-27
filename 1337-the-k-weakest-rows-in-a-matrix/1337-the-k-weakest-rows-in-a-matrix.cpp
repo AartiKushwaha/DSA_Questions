c0e512512018c7699a23b99cf3f0ae1099c1d5aa
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-03-27 09:57:29 +0530

    Time: 28 ms (11.08%), Space: 11 MB (7.93%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
+    
+    int countOnes(vector<int> nums){
+        int n = nums.size();
+        int low = 0, high = n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid = high - (high-low)/2;
+            if(nums[mid]>0){
+                low = mid+1;
+            }
+            else{
+                ans = mid;
+                high = mid-1;
+            }
+        }
+        if(ans==-1) return n;
+        else return ans;
+    }
+    
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         set<pair<int, int>> m;
-        for (auto i = 0; i < mat.size(); ++i) {
-            auto p = accumulate(begin(mat[i]), end(mat[i]), 0);
+        for (int i = 0; i < mat.size(); ++i) {
+            int p = countOnes(mat[i]);
             m.insert({p, i});
         }
         vector<int> res;
