86d2a6371c62ab1d511991bd8467425835ad3ef3
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2022-04-24 12:38:19 +0530

    Time: 8 ms (61.15%), Space: 9.1 MB (23.44%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     
-    int solver(vector<vector<int>>& triangle, int n, int i, int j, vector<vector<int>>& memo){
-        if(i==n-1) return triangle[n-1][j];
-        
-        if(memo[i][j] != -1) return memo[i][j];
-        int down = triangle[i][j] + solver(triangle, n, i+1, j, memo);
-        int diag = triangle[i][j] + solver(triangle, n, i+1, j+1, memo);
-        
-        return memo[i][j] = min(down, diag);
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> memo(n, vector<int>(n, -1));
-        return solver(triangle, n, 0, 0, memo);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i=0; i<n; i++) dp[n-1][i] = triangle[n-1][i];
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diag);
+            }
+        }
+        return dp[0][0];
     }
 };
